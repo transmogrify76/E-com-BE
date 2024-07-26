788b5e982695bd2f304b70dbe68ae856ec27bcd7
ShubhamPrasad-TransEV <transmogrify11@outlook.com>
2024-07-26 17:02:29 +0530

    Merge conflict fixed

--- EcommerseBackend/src/app.js
+++ EcommerseBackend/src/app.js
@@ -1,12 +1,8 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
-<<<<<<< Updated upstream
-import path from "path"
-=======
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from './swagger-output.json' assert { type: 'json' };
->>>>>>> Stashed changes
 
 const app = express ()
 
@@ -29,40 +25,10 @@ import userRouter from './routes/user.routes.js'
 import sellerRouter from './routes/seller.routes.js'
 import AdminRouter from './routes/admin.routes.js'
 
-<<<<<<< Updated upstream
-//temp image upload logic
-import multer from 'multer';
-
-// Set up storage for uploaded files
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads/'); // Directory to save uploaded files
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + path.extname(file.originalname)); // Append timestamp to the original filename
-    }
-});
-
-// Initialize multer with the storage configuration
-const upload = multer({ storage });
-
-// Create an endpoint for uploading images
-app.post('/images/uploads', upload.single('image'), (req, res) => {
-    if (!req.file) {
-        return res.status(400).send('No file uploaded.');
-    }
-    res.status(200).send({
-        message: 'File uploaded successfully',
-        file: req.file
-    });
-});
-
-=======
 // Define a route for the root URL
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
->>>>>>> Stashed changes
 
 
 //routes declaration
